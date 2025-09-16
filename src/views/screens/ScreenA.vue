2a73575e946efc59eb7c760cfcbb894313a99611
Lings23 <2674163492@qq.com>
2025-09-16 11:27:44 +0800

    Changes

---
+++ src/views/screens/ScreenA.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="screen">
+    <div class="title">{{ title }}</div>
+    <div class="content">
+      <LeftTop />
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftTop from './system3D.vue';
+export default {
+  name: 'ScreenA',
+  computed: {
+    title() {
+      return (this.$route.meta && this.$route.meta.title) || '大屏 A';
+    }
+  },
+  components: {
+    LeftTop
+  }
+}
+</script>
+
+<style scoped>
+.screen {
+  width: 100%;
+  height: 100%;
+  color: #fff;
+}
+.title {
+  text-align: center;
+  font-size: 24px;
+  margin: 8px 0 12px;
+}
+</style>
+
+
---
+++ src/views/screens/ScreenB.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="screen">
+    <div class="title">{{ title }}</div>
+    <div class="content">在这里搭建你的新大屏模块...</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScreenB',
+  computed: {
+    title() {
+      return (this.$route.meta && this.$route.meta.title) || '大屏 B';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.screen {
+  width: 100%;
+  height: 100%;
+  color: #fff;
+}
+.title {
+  text-align: center;
+  font-size: 24px;
+  margin: 8px 0 12px;
+}
+</style>
+
+
